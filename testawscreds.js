45411ced556b8355fc63abd2cf0670f1dbb91167
EC2 Default User <ec2-user@ip-172-31-35-29.us-west-2.compute.internal>
2014-03-16 21:40:27 +0000

    Initial commit

---
+++ testawscreds.js
@@ -0,0 +1,13 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-west-2'});
+console.log("AWS SDK Version=", JSON.stringify(AWS.VERSION));
+console.log("Credentials=", JSON.stringify(AWS.config.credentials));
+if(AWS.config.credentials != undefined) {
+	console.log("SecretKey=", JSON.stringify(AWS.config.credentials.secretAccessKey));
+}
+
+var s3 = new AWS.S3();
+
+var signedUrl = s3.getSignedUrl('getObject', {Bucket: 'test-bucket', Key: 'test-key'});
+console.log("SignedUrl=", JSON.stringify(signedUrl));
