7e47b3f8b19d71298150bc403d6fe5274385c87f
EC2 Default User <ec2-user@ip-172-31-35-29.us-west-2.compute.internal>
2014-03-17 02:03:29 +0000

    Fixed based on answer from lsegal

--- testawscreds.js
+++ testawscreds.js
@@ -2,12 +2,23 @@ var AWS = require('aws-sdk');
 
 AWS.config.update({region: 'us-west-2'});
 console.log("AWS SDK Version=", JSON.stringify(AWS.VERSION));
-console.log("Credentials=", JSON.stringify(AWS.config.credentials));
-if(AWS.config.credentials != undefined) {
-	console.log("SecretKey=", JSON.stringify(AWS.config.credentials.secretAccessKey));
+
+AWS.config.getCredentials(useCredentials);
+
+function useCredentials(err) {
+	if(err) throw err;
+	console.log("Credentials=", JSON.stringify(AWS.config.credentials));
+	if(AWS.config.credentials != undefined) {
+		console.log("SecretKey=", JSON.stringify(AWS.config.credentials.secretAccessKey));
+	}
+	var s3 = new AWS.S3();
+
+	s3.getSignedUrl('getObject', {Bucket: 'test-bucket', Key: 'test-key'}, useSignedUrl);
+
+	function useSignedUrl(err, url) {
+		if(err) throw err;
+
+		console.log("SignedUrl=", JSON.stringify(url));
+	}
+
 }
-
-var s3 = new AWS.S3();
-
-var signedUrl = s3.getSignedUrl('getObject', {Bucket: 'test-bucket', Key: 'test-key'});
-console.log("SignedUrl=", JSON.stringify(signedUrl));
